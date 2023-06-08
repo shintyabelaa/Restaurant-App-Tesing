5fcd299d500bd98640ea6d49923b863e1b663925
Desak Made Shintya Belardin <125622177+shintyabelaa@users.noreply.github.com>
2023-06-08 13:59:12 +0700

    first commit

--- e2e/Favorite_Restaurants.spec.js
+++ e2e/Favorite_Restaurants.spec.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-undef */
+const assert = require('assert');
+
 Feature('Favorite Restaurants');
 
 Before(({ I }) => {
@@ -8,4 +10,26 @@ Before(({ I }) => {
 Scenario('showing empty favorite restaurants', ({ I }) => {
   I.seeElement('#query');
   I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+});
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.restaurant__name a');
+
+  const firstResto = locate('.restaurant__name a').first();
+  const firstRestoName = await I.grabTextFrom(firstResto);
+  I.click(firstResto);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  pause();
+  I.seeElement('.restaurant-item');
+  const likedRestoName = await I.grabTextFrom('.restaurant__name');
+
+  assert.strictEqual(firstRestoName, likedRestoName);
 });
--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,42 +1,42 @@
 import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
-// import FavoriteRestaurantSearchView from './liked-movies/favorite-movie-search-view';
-// import FavoriteRestaurantShowPresenter from './liked-movies/favorite-restaurant-show-presenter';
-// import FavoriteRestaurantSearchPresenter from './liked-movies/favorite-restaurant-search-presenter';
+import FavoriteRestaurantSearchView from './liked-restaurants/favorite-restaurant-search-view';
+import FavoriteRestaurantShowPresenter from './liked-restaurants/favorite-restaurant-show-presenter';
+import FavoriteRestaurantSearchPresenter from './liked-restaurants/favorite-restaurant-search-presenter';
 
-// const view = new FavoriteRestaurantSearchView();
-
-// const Favorite = {
-//   async render() {
-//     return view.getTemplate();
-//   },
-
-//   async afterRender() {
-//     new FavoriteRestaurantShowPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb});
-//     new FavoriteRestaurantSearchPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb});
-//   },
-// };
-
-import { createRestaurantListTemplate } from '../templates/template-creator';
+const view = new FavoriteRestaurantSearchView();
 
 const Favorite = {
   async render() {
-    return `
-      <div class="content">
-        <h2 class="explore__label">Your Favorite Restaurants</h2>
-        <div id="restaurants" class="restaurants">
-        </div>
-      </div>
-    `;
+    return view.getTemplate();
   },
 
   async afterRender() {
-    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
-    const restaurantsContainer = document.querySelector('#restaurants');
-
-    restaurants.forEach((restaurant) => {
-      restaurantsContainer.innerHTML += createRestaurantListTemplate(restaurant);
-    });
+    new FavoriteRestaurantShowPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb });
+    new FavoriteRestaurantSearchPresenter({ view, favoriteRestaurants: FavoriteRestaurantIdb });
   },
 };
+
+// import { createRestaurantListTemplate } from '../templates/template-creator';
+
+// const Favorite = {
+//   async render() {
+//     return `
+//       <div class="content">
+//         <h2 class="explore__label">Your Favorite Restaurants</h2>
+//         <div id="restaurants" class="restaurants">
+//         </div>
+//       </div>
+//     `;
+//   },
+
+//   async afterRender() {
+//     const restaurants = await FavoriteRestaurantIdb.getAllRestaurants();
+//     const restaurantsContainer = document.querySelector('#restaurants');
+
+//     restaurants.forEach((restaurant) => {
+//       restaurantsContainer.innerHTML += createRestaurantListTemplate(restaurant);
+//     });
+//   },
+// };
 
 export default Favorite;
